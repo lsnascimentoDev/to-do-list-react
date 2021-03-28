307b1eba57499b33c55a45a96614fe65e2da676c
Lucas.Nascimento <lsnascimento.dev@gmail.com>
2021-03-27 22:33:33 -0300

    readme

---
+++ readme.md
@@ -0,0 +1,53 @@
+<h1 align="center" style=  >
+to.do tasks
+</h1>
+
+<h1>
+<img src="https://ik.imagekit.io/jn5i7lyh9ft/to-do_cEFOhugSmM.png?tr=w-1104,h-736,fo-auto" align="center"  />
+</h1>
+
+# Indice
+
+- [Sobre](#-sobre)
+- [Tecnologias Utilizadas](#-tecnologias-utilizadas)
+- [Como baixar o projeto](#-como-baixar-o-projeto)
+
+
+## ✍ Sobre
+
+O projeto **to.do tasks**  é uma alternativa simples e eficaz para gerenciar atividades a fazer durante o dia.
+
+---
+
+## 🔧 Tecnologias utilizadas
+
+
+- [ReactJS](https://reactjs.org)
+- [TypeScript](https://www.typescriptlang.org)
+- [Jest](https://jestjs.io/pt-BR/)
+
+---
+
+## ⚡ Como baixar o projeto
+</br>
+
+```bash
+
+# Clonar o repositório
+$ git clone https://github.com/lsnascimentoDev/to-do-list-react.git
+
+# Entrar no diretório
+$ cd to-do-list-react
+
+# Instalar as dependências
+$ yarn install
+
+# Iniciar o projeto
+$ yarn dev
+```
+
+
+---
+
+Desenvolvido por Lucas Nascimento 💪
+
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,7 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // Criando uma nova task com um id random, não permitindo criar caso o título seja vazio.
 
     if(!newTaskTitle) return;
 
@@ -31,7 +31,7 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    // Alterando entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const newTasks = tasks.map(task => task.id === id ? {
       ...task,
       isComplete: !task.isComplete
@@ -41,7 +41,7 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    // Removendo uma task da listagem pelo ID
     const filteredTasks = tasks.filter(task => task.id !== id);
 
     setTasks(filteredTasks);
